c30b60c60b6692e8f367642a23c35ed7402bb4d7
AnastasiaZhukova <azhuk.mail@gmail.com>
2017-10-10 16:30:04 +0300

    Fully implemented basic classes and interfaces

--- app/src/main/java/com/github/anastasiazhukova/calculator/Value.java
+++ app/src/main/java/com/github/anastasiazhukova/calculator/Value.java
@@ -2,28 +2,95 @@ package com.github.anastasiazhukova.calculator;
 
 public class Value implements IValue {
 
+    private String mValue;
+    private static final String defaultZeroValue = "0";
+
+    Value() {
+        mValue = defaultZeroValue;
+    }
+
+    Value(final double pDoubleValue) {
+        if (pDoubleValue == 0) {
+            mValue = defaultZeroValue;
+        } else {
+            mValue = String.valueOf(pDoubleValue);
+        }
+    }
+
+    void setValue(final double pDoubleValue) {
+        if (pDoubleValue == 0) {
+            mValue = defaultZeroValue;
+        } else {
+            mValue = String.valueOf(pDoubleValue);
+        }
+    }
+
     @Override
     public void Append(char ch) {
 
+        StringBuilder stringBuilder = new StringBuilder(mValue);
+        if (ConvertToNumber() == 0) {
+            if (ch != '.') {
+                if (!isDoubleValue()) {
+                    stringBuilder = new StringBuilder("");
+                }
+            }
+        }
+        stringBuilder.append(ch);
+        mValue = stringBuilder.toString();
     }
 
     @Override
     public void RemoveLast() {
-
+        if (ConvertToNumber() == 0) {
+            //checks if there is a dot
+            if (!isDoubleValue()) {
+                return;
+            }
+        }
+        StringBuilder stringBuilder = new StringBuilder(mValue);
+        stringBuilder = stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+        mValue = stringBuilder.toString();
+        if (isZeroValue()) {
+            mValue = defaultZeroValue;
+        } else {
+            mValue = stringBuilder.toString();
+        }
     }
 
     @Override
     public double ConvertToNumber() {
-        return 0;
+        if (isZeroValue()) {
+            return 0;
+        } else {
+            return Double.valueOf(mValue);
+        }
     }
 
     @Override
     public String ConvertToString() {
-        return null;
+        return mValue;
     }
 
     @Override
     public void changeSign() {
+        final char sign = mValue.charAt(0);
+        StringBuilder stringBuilder = new StringBuilder(mValue);
+        stringBuilder = stringBuilder.reverse();
+        if (sign == '-') {
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+        } else {
+            stringBuilder.append('-');
+        }
+        mValue = (stringBuilder.reverse()).toString();
 
+    }
+
+    private boolean isDoubleValue() {
+        return mValue.contains(".");
+    }
+
+    boolean isZeroValue() {
+        return mValue.isEmpty() || mValue == "-" || Double.valueOf(mValue) == 0;
     }
 }
